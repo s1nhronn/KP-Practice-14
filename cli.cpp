324fc753578446046145825485a62eae34e9f853
s1nhronn <silekatik@yandex.ru>
2025-12-25 15:43:29 +0300

    Add implementation for getword

--- cli.cpp
+++ cli.cpp
@@ -1,6 +1,9 @@
+#include <cassert>
 #include <cctype>
+#include <ios>
 #include <iostream>
 #include <istream>
+#include <stdexcept>
 
 void hi()
 {
@@ -12,7 +15,26 @@ bool is_space(char c)
   return std::isspace(c);
 }
 
-std::istream &getword(std::istream &os, char *word, size_t k, bool (*c)(char));
+std::istream &getword(std::istream &is, char *word, size_t k, bool (*c)(char))
+{
+  assert(k > 0 && "k must be greater than 0");
+  if (!k || !word)
+  {
+    throw std::logic_error("bad buffer");
+  }
+  is >> std::noskipws;
+  size_t i = 0;
+  for (char next = 0; is && !c(next) && (i < k - 1); ++i, word[i] = next)
+  {
+    is >> next;
+    word[i] = next;
+  }
+  if (i == k)
+  {
+    is.clear(is.rdstate() | std::ios::failbit);
+  }
+  return is >> std::skipws;
+}
 
 size_t match(const char *word, const char *const *words, size_t k);
 
