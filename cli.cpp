ed08ce942ab090504d5fff40f4550586aff5653f
s1nhronn <silekatik@yandex.ru>
2025-12-25 15:03:26 +0300

    Edit cli.cpp

--- cli.cpp
+++ cli.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <iterator>
+
+void hi()
+{
+  std::cout << "<Hi!>\n";
+}
 
 int main()
 {
-  std::cout << "CLI\n";
+  constexpr size_t cmds_count = 1;
+  void (*cmds[1])() = {hi};
+  size_t i = 0;
+  while (!(std::cin >> i).eof())
+  {
+    if (std::cin.fail())
+    {
+      std::cerr << "< INVALID COMMAND >\n";
+      return 1;
+    }
+    else if (i < cmds_count)
+    {
+      cmds[i]();
+    }
+    else
+    {
+      std::cerr << "< UNKNOWN COMMAND >\n";
+    }
+  }
 }
